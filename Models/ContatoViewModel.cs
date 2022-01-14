76f08b9b2c566032bfbe99d8978b9baefefefe2d
Thaysa <83955839+ThaysaRafaele@users.noreply.github.com>
2022-01-14 12:37:16 -0400

    Add files via upload

---
+++ Models/ContatoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    public class ContatoViewModel
+    {     
+        //propriedades -> Prop + TAB + TAB
+        public int ContatoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public virtual EnderecoViewModel Endereco { get; set; }
+        public virtual int EnderecoId { get; set; }        
+    }
+}
---
+++ Models/EnderecoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    public class EnderecoViewModel
+    {
+        //propriedades -> Prop + TAB + TAB
+        public int EnderecoId { get; set; }
+        public string Local { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }      
+    }
+}
